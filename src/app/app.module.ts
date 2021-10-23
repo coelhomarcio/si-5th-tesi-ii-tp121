9e23220426caed8593d79228daa7616f5845ef98
Marcio Coelho <marciocoelho@gmail.com>
2021-10-23 11:39:40 -0300

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule }                from "@angular/core";
-import { BrowserModule }           from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule }                         from "@angular/core";
+import { BrowserModule }                    from "@angular/platform-browser";
+import { BrowserAnimationsModule }          from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AngularFireModule }                from "@angular/fire/compat";
+import { AngularFireAuthModule }            from "@angular/fire/compat/auth";
 
-import { MatIconModule } from "@angular/material/icon";
+import { MatIconModule }      from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule }     from "@angular/material/input";
+import { MatButtonModule }    from "@angular/material/button";
+import { MatDividerModule }   from "@angular/material/divider";
 
 import { AppRoutingModule } from "./app-routing.module";
 
@@ -10,6 +17,15 @@ import { AppComponent }    from "./app.component";
 import { HeaderComponent } from "./component/header/header.component";
 import { FooterComponent } from "./component/footer/footer.component";
 import { HomeComponent }   from "./component/home/home.component";
+
+const firebaseConfig = {
+	apiKey:            "AIzaSyCr8SE_46FSgP_VsfFxFejy1gUZAIwKQuI",
+	authDomain:        "si-5th-tesi-ii-tp121.firebaseapp.com",
+	projectId:         "si-5th-tesi-ii-tp121",
+	storageBucket:     "si-5th-tesi-ii-tp121.appspot.com",
+	messagingSenderId: "77959390699",
+	appId:             "1:77959390699:web:5e66fa07cf679dbe9ae5ae"
+};
 
 @NgModule({
 	declarations: [
@@ -21,7 +37,15 @@ import { HomeComponent }   from "./component/home/home.component";
 	imports:      [
 		BrowserModule,
 		BrowserAnimationsModule,
+		FormsModule,
+		ReactiveFormsModule,
+		AngularFireModule.initializeApp(firebaseConfig),
+		AngularFireAuthModule,
 		MatIconModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatDividerModule,
 		AppRoutingModule
 	],
 	providers:    [],
