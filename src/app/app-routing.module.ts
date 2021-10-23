d16a611edbcddc7d7fca1f25905268ef21a1b9d2
Marcio Coelho <marciocoelho@gmail.com>
2021-10-23 14:28:18 -0300

    Updated app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./component/home/home.component";
 
 const routes: Routes = [
-	{ path: "", redirectTo: "home", pathMatch: "full" },
-	{ path: "home", component: HomeComponent }
+	{ path: "", component: HomeComponent }
 ];
 
 @NgModule({
