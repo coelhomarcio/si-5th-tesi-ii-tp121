bd7d7f663fecd7cb1fdac3f2ab932653c9acc024
Marcio Coelho <marciocoelho@gmail.com>
2021-10-22 23:17:33 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }                from "@angular/core";
+import { BrowserModule }           from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MatIconModule } from "@angular/material/icon";
+
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AppComponent }    from "./app.component";
+import { HeaderComponent } from "./component/header/header.component";
+import { FooterComponent } from "./component/footer/footer.component";
+import { HomeComponent }   from "./component/home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		HomeComponent
+	],
+	imports:      [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatIconModule,
+		AppRoutingModule
+	],
+	providers:    [],
+	bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
